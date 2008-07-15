0117ddc0a2ff37c6da9a1c9a4dbf60bff57ba152
Vidar Wahlberg <canidae@exent.net>
2008-07-15 16:52:37 +0200

    whoops, typo

--- Globals.h
+++ Globals.h
@@ -129,7 +129,7 @@
 #define MESSAGE_STAIRCASE_DOWN "There is a staircase down here.  "
 #define MESSAGE_OPEN_DOOR "There is an open door here.  "
 #define MESSAGE_CHOOSE_DIRECTION "In what direction?  "
-#define MESSAGE_YOU_SEE_HERE "You see here " // not two spaces here as it's followed by eg. "an lichen corpse"
+#define MESSAGE_YOU_SEE_HERE "You see here " // not two spaces here as it's followed by eg. "a lichen corpse"
 #define MESSAGE_THINGS_THAT_ARE_HERE "Things that are here:  "
 #define MESSAGE_PICK_UP_WHAT "Pick up what?  "
 
