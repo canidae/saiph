63fd0635c267388da96ee0183162af73062cd809
Vidar Wahlberg <canidae@exent.net>
2008-07-25 19:47:28 +0200

    just added some comments in the analyzer

--- Analyzer.cpp
+++ Analyzer.cpp
@@ -13,20 +13,31 @@ Analyzer::~Analyzer() {
 
 /* methods */
 void Analyzer::begin() {
+	/* decisions should be made here. */
 }
 
 void Analyzer::command(string *command) {
+	/* this one should be fixed.
+	 * it should return pointer to a string instead of this */
 }
 
 void Analyzer::finish() {
+	/* here we should only set up action depending decisions made earlier.
+	 * any requests sent here may be delayed 1 turn! */
 }
 
 void Analyzer::inspect(const Point &point) {
+	/* this might not be needed in the future */
 }
 
 void Analyzer::parseMessages(const string &messages) {
+	/* simply parse messages */
 }
 
 bool Analyzer::request(const Request &request) {
+	/* respond to requests from other analyzers.
+	 * this is the reason we shouldn't make decisions in finish,
+	 * because when we do, another analyzer may already have done
+	 * its finish method */
 	return false;
 }
