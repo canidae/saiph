76cf4e3e5eec683fb8af1b23afb898d6ca582c6f
Vidar Wahlberg <canidae@exent.net>
2008-07-30 23:57:20 +0200

    whoops, minor mistake earlier tonight

--- Monster.cpp
+++ Monster.cpp
@@ -1,5 +1,5 @@
 #include "Monster.h"
 
 /* constructors */
-Monster::Monster(unsigned char symbol, int color, bool visible) : symbol(symbol), color(color), visible(visible), friendly(friendly) {
+Monster::Monster(unsigned char symbol, int color, bool visible, bool friendly) : symbol(symbol), color(color), visible(visible), friendly(friendly) {
 }
