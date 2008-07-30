8036425e9c9f2a41fcf95791ba8946a1fc7b729c
Vidar Wahlberg <canidae@exent.net>
2008-07-31 00:13:01 +0200

    added new analyzer, wand. only picks up wands so far (in theory), but better than letting monsters have them. also, fodder for those bloody nymphs

---
+++ Analyzers/Wand.cpp
@@ -0,0 +1,33 @@
+#include "Wand.h"
+
+/* constructors */
+Wand::Wand(Saiph *saiph) : Analyzer("Wand"), saiph(saiph) {
+}
+
+/* methods */
+void Wand::finish() {
+	if (saiph->on_ground != NULL) {
+		/* if there's a wand here, pick it up */
+		for (list<Item>::iterator i = saiph->on_ground->items.begin(); i != saiph->on_ground->items.end(); ++i) {
+			if (i->name.substr(0, 4) != "wand" || i->name.substr(i->name.size() - 4) != "wand")
+				continue; // not a wand
+			/* request that someone loot this stash */
+			req.request = REQUEST_LOOT_STASH;
+			req.priority = WAND_PICKUP_PRIORITY;
+			req.coordinate = saiph->position;
+			saiph->request(req);
+			break;
+		}
+	}
+}
+
+void Wand::parseMessages(const string &messages) {
+	if (saiph->world->menu && messages.find(MESSAGE_PICK_UP_WHAT, 0) != string::npos) {
+		for (map<unsigned char, Item>::iterator p = saiph->pickup.begin(); p != saiph->pickup.end(); ++p) {
+			if (p->second.name.substr(0, 4) != "wand" || p->second.name.substr(p->second.name.size() - 4) != "wand")
+				continue; // not a wand
+			command = p->first;
+			priority = PRIORITY_PICKUP_ITEM;
+		}
+	}
+}
---
+++ Analyzers/Wand.h
@@ -0,0 +1,35 @@
+#ifndef WAND_H
+/* defines */
+#define WAND_H
+/* priorities */
+#define WAND_PICKUP_PRIORITY 425
+
+/* forward declare */
+class Wand;
+
+/* includes */
+#include <string>
+#include "../Analyzer.h"
+#include "../Globals.h"
+#include "../Request.h"
+#include "../Saiph.h"
+
+/* namespace */
+using namespace std;
+
+/* monitors health */
+class Wand : public Analyzer {
+	public:
+		/* constructors */
+		Wand(Saiph *saiph);
+
+		/* methods */
+		void finish();
+		void parseMessages(const string &messages);
+
+	private:
+		/* variables */
+		Saiph *saiph;
+		Request req;
+};
+#endif
