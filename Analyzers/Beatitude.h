bec13ca26e1e5e1c37b8b760aabc66e63d0203e7
Vidar Wahlberg <canidae@exent.net>
2008-08-13 08:11:27 +0200

    more aggressive at checking beatitude

--- Analyzers/Beatitude.h
+++ Analyzers/Beatitude.h
@@ -6,7 +6,7 @@
  * BEATITUDE_DROP_ALTAR_ADD_PER_1000_MOVE says how many additional items per
  * move we need before we bother heading towards an altar. we do this so that
  * she's a bit more reluctant to go a long way back to an altar */
-#define BEATITUDE_DROP_ALTAR_MIN 2
+#define BEATITUDE_DROP_ALTAR_MIN 1
 #define BEATITUDE_DROP_ALTAR_ADD_PER_1000_MOVE 64
 /* priorities */
 #define BEATITUDE_DROP_ALTAR_PRIORITY 225
