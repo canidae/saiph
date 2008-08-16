f40c08a8455d3f7f56c9f54543846b5b8ea8b52c
Vidar Wahlberg <canidae@exent.net>
2008-08-17 00:39:58 +0200

    hmm, don't think this is needed

--- Analyzers/Enhance.cpp
+++ Analyzers/Enhance.cpp
@@ -8,7 +8,6 @@ Enhance::Enhance(Saiph *saiph) : Analyzer("Enhance"), saiph(saiph), do_enhance(t
 void Enhance::parseMessages(const string &messages) {
 	if (messages.find(ENHANCE_MESSAGE_CONFIDENT, 0) != string::npos) {
 		do_enhance = true;
-		return;
 	} else if (messages.find(ENHANCE_CURRENT_SKILLS, 0) != string::npos || messages.find(ENHANCE_PICK_A_SKILL, 0) != string::npos) {
 		got_enhance_menu = true;
 	}
@@ -16,12 +15,10 @@ void Enhance::parseMessages(const string &messages) {
 		/* no longer showing a menu, make those booleans false */
 		do_enhance = false;
 		got_enhance_menu = false;
-		return;
 	} else if (do_enhance && !saiph->world->menu) {
 		/* request that the enhance menu is shown */
 		command = ENHANCE_ENHANCE;
 		priority = PRIORITY_LOOK;
-		return;
 	} else if (got_enhance_menu && saiph->world->menu) {
 		/* showing enhance menu.
 		 * enhance 'a' for now.
@@ -31,6 +28,5 @@ void Enhance::parseMessages(const string &messages) {
 		else
 			command = " ";
 		priority = PRIORITY_CONTINUE_ACTION;
-		return;
 	}
 }
