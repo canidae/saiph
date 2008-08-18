6714317eb6394fab33bcf28f58c0f2b728b8d968
Vidar Wahlberg <canidae@exent.net>
2008-08-18 23:59:52 +0200

    apparently we can have "partly eaten" by stuff that takes 1 turn to eat. intriguing

--- Analyzers/Food.cpp
+++ Analyzers/Food.cpp
@@ -4,21 +4,29 @@
 Food::Food(Saiph *saiph) : Analyzer("Food"), saiph(saiph) {
 	command2 = "";
 	priority = ILLEGAL_PRIORITY;
-	//eat_order.push_back("tin");
+	eat_order.push_back("partly eaten meatball");
 	eat_order.push_back("meatball");
+	eat_order.push_back("partly eaten meat ring");
 	eat_order.push_back("meat ring");
+	eat_order.push_back("partly eaten meat stick");
 	eat_order.push_back("meat stick");
 	eat_order.push_back("partly eaten huge chunk of meat");
 	eat_order.push_back("huge chunk of meat");
+	eat_order.push_back("partly eaten cream pie");
 	eat_order.push_back("cream pie");
 	eat_order.push_back("partly eaten lichen corpse");
 	eat_order.push_back("lichen corpse");
 	eat_order.push_back("partly eaten tripe ration");
 	eat_order.push_back("tripe ration");
+	eat_order.push_back("partly eaten melon");
 	eat_order.push_back("melon");
+	eat_order.push_back("partly eaten apple");
 	eat_order.push_back("apple");
+	eat_order.push_back("partly eaten pear");
 	eat_order.push_back("pear");
+	eat_order.push_back("partly eaten kelp frond");
 	eat_order.push_back("kelp frond");
+	eat_order.push_back("partly eaten C-ration");
 	eat_order.push_back("C-ration");
 	eat_order.push_back("partly eaten cram ration");
 	eat_order.push_back("cram ration");
@@ -26,22 +34,33 @@ Food::Food(Saiph *saiph) : Analyzer("Food"), saiph(saiph) {
 	eat_order.push_back("food ration");
 	eat_order.push_back("partly eaten gunyoki");
 	eat_order.push_back("gunyoki");
+	eat_order.push_back("partly eaten K-ration");
 	eat_order.push_back("K-ration");
+	eat_order.push_back("partly eaten clove of garlic");
 	eat_order.push_back("clove of garlic");
+	eat_order.push_back("partly eaten fortune cookie");
 	eat_order.push_back("fortune cookie");
+	eat_order.push_back("partly eaten banana");
 	eat_order.push_back("banana");
+	eat_order.push_back("partly eaten orange");
 	eat_order.push_back("orange");
+	eat_order.push_back("partly eaten candy bar");
 	eat_order.push_back("candy bar");
+	eat_order.push_back("partly eaten byte");
 	eat_order.push_back("byte");
+	eat_order.push_back("partly eaten lump of royal jelly");
 	eat_order.push_back("lump of royal jelly");
 	eat_order.push_back("partly eaten pancake");
 	eat_order.push_back("pancake");
 	eat_order.push_back("partly eaten lembas wafer");
 	eat_order.push_back("lembas wafer");
+	eat_order.push_back("partly eaten carrot"); // will cure blindness
 	eat_order.push_back("carrot"); // will cure blindness
+	eat_order.push_back("partly eaten eucalyptus leaf"); // will cure sickness
 	eat_order.push_back("eucalyptus leaf"); // will cure sickness
+	eat_order.push_back("partly eaten sprig of wolfsbane"); // will cure lycanthropy
 	eat_order.push_back("sprig of wolfsbane"); // will cure lycanthropy
-	eat_order.push_back("partly eaten lizard corpse"); // wil cure stoning?
+	eat_order.push_back("partly eaten lizard corpse"); // wil cure stoning
 	eat_order.push_back("lizard corpse"); // wil cure stoning
 
 	/* corpses */
