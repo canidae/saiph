4b14469d70debe2dffb35abb24b74d138454c2d7
Vidar Wahlberg <canidae@exent.net>
2009-01-02 17:49:59 +0100

    just fixed some comments

--- Analyzers/Loot.cpp
+++ Analyzers/Loot.cpp
@@ -94,7 +94,7 @@ void Loot::analyze() {
 
 	if (priority >= PRIORITY_LOOT_VISIT_STASH || saiph->world->player.hallucinating || saiph->world->player.blind || saiph->world->player.encumbrance > UNENCUMBERED)
 		return;
-	/* set "visit_old_stash" when we're standing on downstairs */
+	/* go to "visit_old_stash" if it's set */
 	if (visit_old_stash.level >= 0 && visit_old_stash.level < (int) saiph->levels.size()) {
 		map<Point, Stash>::iterator s = saiph->levels[visit_old_stash.level].stashes.find(visit_old_stash);
 		if (s != saiph->levels[visit_old_stash.level].stashes.end()) {
@@ -110,6 +110,7 @@ void Loot::analyze() {
 	}
 	if (saiph->levels[saiph->position.level].dungeonmap[saiph->position.row][saiph->position.col] != STAIRS_DOWN)
 		return;
+	/* set "visit_old_stash" when we're standing on downstairs */
 	min_moves = INT_MAX;
 	for (vector<Level>::size_type level = 0; level < saiph->levels.size(); ++level) {
 		for (map<Point, Stash>::iterator s = saiph->levels[level].stashes.begin(); s != saiph->levels[level].stashes.end(); ++s) {
