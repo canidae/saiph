78fd3b5f1b8ba01b8fbda02f3b5989978b340847
Vidar Wahlberg <canidae@exent.net>
2009-02-07 16:25:52 +0100

    few changes, only made it look like i want it to

--- Analyzers/Rub.h
+++ Analyzers/Rub.h
@@ -1,18 +1,18 @@
 #ifndef RUB_H
 #define	RUB_H
 
+#include <string>
 #include "../Analyzer.h"
 #include "../Saiph.h"
-#include <string>
 
 class Rub : public Analyzer {
 	public:
-		Rub(Saiph* saiph);
-		void parseMessages(const std::string& messages);
+		Rub(Saiph *saiph);
+		void parseMessages(const std::string &messages);
 		void analyze();
 
 	private:
-		Saiph* saiph;
+		Saiph *saiph;
 		unsigned char magic_lamp_key;
 		void lookForMagicLamp();
 };
