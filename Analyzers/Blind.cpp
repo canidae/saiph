e70e2de7051063a9d25708a6b6a1ca9c52efdb0c
Vidar Wahlberg <canidae@exent.net>
2009-02-08 11:09:36 +0100

    fixed it like i want it

--- Analyzers/Blind.cpp
+++ Analyzers/Blind.cpp
@@ -1,28 +1,15 @@
 #include "Blind.h"
-#include "../Saiph.h"
 #include "../World.h"
-#include "../Player.h"
-#include "../Request.h"
 
 using namespace std;
 
-Blind::Blind(Saiph* saiph) : Analyzer("Blind"), saiph(saiph), willful_blindness(false), blinding_tool(0) {
+Blind::Blind(Saiph *saiph) : Analyzer("Blind"), saiph(saiph), willful_blindness(false), blinding_tool(ILLEGAL_ITEM) {
 }
 
 void Blind::analyze() {
-	if (saiph->inventory_changed) {
-		if (blinding_tool != 0 && !isBlindingTool(blinding_tool))
-			blinding_tool = 0;
-		if (blinding_tool == 0) {
-			for (map<unsigned char, Item>::iterator i = saiph->inventory.begin(); i != saiph->inventory.end(); ++i)
-				if (isBlindingTool(i->second)) {
-					blinding_tool == i->first;
-					break;
-				}
-		}
-	}
+	if (saiph->inventory_changed)
+		findBlindingTool();
 	if (saiph->world->player.blind && !willful_blindness) {
-		Request req;
 		req.request = REQUEST_APPLY_UNIHORN;
 		req.priority = PRIORITY_HEALTH_CURE_NON_DEADLY;
 		saiph->request(req);
@@ -31,19 +18,35 @@ void Blind::analyze() {
 
 bool Blind::request(const Request &request) {
 	if (request.request == REQUEST_BECOME_BLIND) {
-		
+		/* TODO: put on blindfold/towel */
 	} else if (request.request == REQUEST_UNBLIND) {
-
+		/* TODO: remove blindfold/towel */
 	}
 	return false;
 }
 
-bool Blind::isBlindingTool(const unsigned char& letter) {
-	map<unsigned char, Item>::iterator iter = saiph->inventory.find(letter);
-	return iter != saiph->inventory.end() && isBlindingTool(iter->second);
+/* private methods */
+void Blind::findBlindingTool() {
+	map<unsigned char, Item>::iterator b = saiph->inventory.find(blinding_tool);
+	if (b != saiph->inventory.end() && isBlindingTool(b->second))
+		return;
+	for (b = saiph->inventory.begin(); b != saiph->inventory.end(); ++b) {
+		if (isBlindingTool(b->second)) {
+			blinding_tool = b->first;
+			return;
+		}
+	}
+	blinding_tool = ILLEGAL_ITEM;
 }
 
-bool Blind::isBlindingTool(const Item& item) {
-	return (item.name == "blindfold" || item.name == "towel") &&
-			(item.beatitude == BLESSED || item.beatitude == UNCURSED);
+bool Blind::isBlindingTool(const Item &item) {
+	if (item.beatitude == CURSED || (item.name != "blindfold" && item.name != "towel"))
+		return false;
+	if (item.beatitude == BEATITUDE_UNKNOWN) {
+		/* must beatify blinding tool first */
+		req.request = REQUEST_BEATIFY_ITEMS;
+		saiph->request(req);
+		return false;
+	}
+	return true;
 }
--- Analyzers/Blind.h
+++ Analyzers/Blind.h
@@ -1,22 +1,23 @@
 #ifndef BLIND_H
 #define BLIND_H
 
-#include "../Saiph.h"
-#include "../Request.h"
 #include "../Analyzer.h"
+#include "../Request.h"
+#include "../Saiph.h"
 
 class Blind : public Analyzer {
-public:
-	Blind(Saiph* saiph);
-	void analyze();
-	bool request(const Request &request);
-	bool isBlindingTool(const unsigned char& letter);
-	bool isBlindingTool(const Item& item);
-private:
-	Saiph* saiph;
-	bool willful_blindness;
-	unsigned char blinding_tool;
+	public:
+		Blind(Saiph *saiph);
+
+		void analyze();
+		bool request(const Request &request);
+		void findBlindingTool();
+		bool isBlindingTool(const Item &item);
+
+	private:
+		Saiph *saiph;
+		Request req;
+		bool willful_blindness;
+		unsigned char blinding_tool;
 };
-
 #endif	
-
