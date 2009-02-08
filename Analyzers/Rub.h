5636406100ce2ec6d37f860b5aa8f488a042a9d2
Vidar Wahlberg <canidae@exent.net>
2009-02-08 11:58:00 +0100

    made Rub suit my taste better

--- Analyzers/Rub.h
+++ Analyzers/Rub.h
@@ -3,17 +3,24 @@
 
 #include <string>
 #include "../Analyzer.h"
-#include "../Saiph.h"
+#include "../Request.h"
+
+class Item;
+class Saiph;
 
 class Rub : public Analyzer {
 	public:
 		Rub(Saiph *saiph);
-		void parseMessages(const std::string &messages);
+
 		void analyze();
+		void parseMessages(const std::string &messages);
 
 	private:
 		Saiph *saiph;
+		Request req;
 		unsigned char magic_lamp_key;
-		void lookForMagicLamp();
+
+		void findMagicLamp();
+		bool isMagicLamp(const Item &item);
 };
 #endif
