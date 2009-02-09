fa380fd84f66c93c7e4a9edaa21779b249723d11
Jeffrey Bosboom <doctorsunglasses@dslextreme.com>
2009-02-09 11:37:39 -0800

    Completed Blind

--- Analyzers/Blind.h
+++ Analyzers/Blind.h
@@ -11,7 +11,9 @@ class Blind : public Analyzer {
 	public:
 		Blind(Saiph *saiph);
 
+		void parseMessages(const std::string &messages);
 		void analyze();
+		void complete();
 		bool request(const Request &request);
 
 	private:
@@ -19,6 +21,7 @@ class Blind : public Analyzer {
 		Request req;
 		bool willful_blindness;
 		unsigned char blinding_tool;
+		int blind_priority, unblind_priority;
 
 		void findBlindingTool();
 		bool isBlindingTool(const Item &item);
