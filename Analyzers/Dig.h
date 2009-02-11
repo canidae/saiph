fcd491e55a50e59bc38ab08625eaa6c5f2807503
Vidar Wahlberg <canidae@exent.net>
2009-02-12 00:49:28 +0100

    canidaeitized Dig
    although, i broke this analyzer quite much in the process. for example,
    it won't dig narrow passes when she stands next to one. also, i
    significantly lowered the priority, and she won't start digging before
    she's done exploring the level and is either just about to search
    desperately or is heading for the downstairs.
    on top of that, did not get time to actually test the changes

--- Analyzers/Dig.h
+++ Analyzers/Dig.h
@@ -1,8 +1,11 @@
 #ifndef DIG_H
 #define	DIG_H
 
+#include <list>
 #include <string>
 #include "../Analyzer.h"
+#include "../Point.h"
+#include "../Request.h"
 
 class Item;
 class Saiph;
@@ -11,21 +14,24 @@ class Dig : public Analyzer {
 	public:
 		Dig(Saiph *saiph);
 
-		void parseMessages(const std::string &message);
 		void analyze();
+		void parseMessages(const std::string &message);
 
 	private:
+		Request req;
 		Saiph *saiph;
-		unsigned char dig_direction;
 		unsigned char digging_tool;
+		unsigned char dig_direction;
+		std::list<Point> dig_locations;
+
+		void findDiggingTool();
+		bool isDiggingTool(const Item &item);
 
 		int directionIs(int direction);
 		bool directionIsWall(int direction);
 		bool directionIsFloor(int direction);
 		int boulderInDirection();
-		bool isDiggingTool(const Item &i);
 		bool isDiggingTool(unsigned char letter);
-		unsigned char findDiggingTool();
 		bool freeWeaponHand();
 };
 #endif	
