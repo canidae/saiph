2fe7bd71b85136446503e2345fd81efa1d6ffefa
Kenny Root <kenny@the-b.org>
2009-02-15 06:30:27 -0600

    Redo door changes.

--- Analyzers/Door.h
+++ Analyzers/Door.h
@@ -1,9 +1,17 @@
 #ifndef DOOR_H
 #define DOOR_H
+
 /* messages */
 #define DOOR_DOOR_LOCKED "  This door is locked.  "
 #define DOOR_UNLOCK_IT "  Unlock it? [yn] (n)  "
 #define DOOR_BREAK_SHOP_DOOR "  \"Cad!  You did 400 zorkmids worth of damage!\"  Pay?  [yn] (n)  "
+#define DOOR_CLOSED_FOR_INVENTORY "\"Closed for inventory.\""
+
+/* door states */
+#define DOOR_LOCKED 1
+#define DOOR_SHOP_INVENTORY 2
+
+#define DOOR_DEBUG_NAME "Door] "
 
 #include <string>
 #include "../Analyzer.h"
