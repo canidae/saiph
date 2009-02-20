504e5d61180fb0ef5821354b336884130816b063
Vidar Wahlberg <canidae@exent.net>
2009-02-20 21:54:03 +0100

    checking in temporary stuff on temporary branch

--- Monster.cpp
+++ Monster.cpp
@@ -1,5 +1,5 @@
 #include "Monster.h"
 
 /* constructors/destructor */
-Monster::Monster(unsigned char symbol, int color, int last_seen, bool visible, int attitude, bool shopkeeper, bool priest, bool minotaur) : symbol(symbol), color(color), last_seen(last_seen), visible(visible), attitude(attitude), shopkeeper(shopkeeper), priest(priest), minotaur(minotaur) {
+Monster::Monster(unsigned char symbol, int color, int last_seen) : symbol(symbol), color(color), last_seen(last_seen) {
 }
