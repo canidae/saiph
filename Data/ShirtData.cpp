12d8010fe0306afbaac7a1334bd699bd0029a8dc
Jeffrey Bosboom <doctorsunglasses@dslextreme.com>
2009-02-27 12:31:02 -0800

    ShirtData::init() and the data within added

--- Data/ShirtData.cpp
+++ Data/ShirtData.cpp
@@ -7,4 +7,11 @@ map<string, ShirtData*> ShirtData::shirts;
 
 ShirtData::ShirtData(const std::string &name, int cost, int weight, int material, int ac, int mc, unsigned long long properties) : ArmorData(name, cost, weight, material, ARMOR_SHIRT, ac, mc, properties) {
 	shirts[name] = this;
+	ArmorData::armors[name] = this;
+	ItemData::items[name] = this;
+}
+
+void ShirtData::init() {
+	ShirtData("Hawaiian shirt", 3, 5, MATERIAL_CLOTH, 0, 0, 0);
+	ShirtData("T-shirt", 2, 5, MATERIAL_CLOTH, 0, 0, 0);
 }
--- Data/ShirtData.h
+++ Data/ShirtData.h
@@ -8,6 +8,7 @@
 class ShirtData : public ArmorData {
 public:
 	static std::map<std::string, ShirtData*> shirts;
+	static void init();
 
 	ShirtData(const std::string &name = "", int cost = 0, int weight = 0, int material = 0, int ac = 0, int mc = 0, unsigned long long properties = 0);
 };
