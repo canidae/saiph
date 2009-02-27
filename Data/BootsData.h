a46f4fc305fc10dd1f0dbf4898646651351e513a
Jeffrey Bosboom <doctorsunglasses@dslextreme.com>
2009-02-27 11:35:22 -0800

    BootsData.h, BootsData.cpp

---
+++ Data/BootsData.h
@@ -0,0 +1,16 @@
+#ifndef _BOOTSDATA_H
+#define	_BOOTSDATA_H
+
+#include <map>
+#include <string>
+#include "ArmorData.h"
+
+class BootsData : public ArmorData {
+public:
+	static std::map<std::string, BootsData*> boots;
+
+	BootsData(const std::string &name, int cost, int weight, int material, int ac, int mc, unsigned long long properties);
+};
+
+#endif	/* _BOOTSDATA_H */
+
