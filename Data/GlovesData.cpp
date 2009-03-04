d48ec3a2e7e864129cc1f1ea737f00972a056bae
Jeffrey Bosboom <doctorsunglasses@dslextreme.com>
2009-03-03 19:38:23 -0800

    Typed in gloves data

--- Data/GlovesData.cpp
+++ Data/GlovesData.cpp
@@ -18,4 +18,8 @@ void GlovesData::create(const std::string &name, int cost, int weight, int mater
 }
 
 void GlovesData::init() {
+	create("leather gloves",          8, 10, MATERIAL_LEATHER, 1, 0, 0);
+	create("gauntlets of dexterity", 50, 10, MATERIAL_LEATHER, 1, 0, PROPERTY_DEXTERITY);
+	create("gauntlets of power",     50, 30, MATERIAL_IRON,    1, 0, PROPERTY_STRENGTH);
+	create("gauntlets of fumbling",  50, 10, MATERIAL_LEATHER, 1, 0, PROPERTY_FUMBLING);
 }
