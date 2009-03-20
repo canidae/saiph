275e4af9b54250fc8fd30993f273b93492c4c44d
Jeffrey Bosboom <doctorsunglasses@dslextreme.com>
2009-03-20 16:34:43 -0700

    Moved PROPERTY_ #defines to Globals.h since we'll be using them in Player.h.

--- Data/ItemData.h
+++ Data/ItemData.h
@@ -5,39 +5,6 @@
 #include <string>
 #include "../Globals.h"
 
-/* properties */
-#define PROPERTY_FIRE              ((unsigned long long) (1LL << 0)) // match MR_FIRE
-#define PROPERTY_COLD              ((unsigned long long) (1LL << 1)) // match MR_COLD
-#define PROPERTY_SLEEP             ((unsigned long long) (1LL << 2)) // match MR_SLEEP
-#define PROPERTY_DISINT            ((unsigned long long) (1LL << 3)) // match MR_DISINT
-#define PROPERTY_SHOCK             ((unsigned long long) (1LL << 4)) // match MR_ELEC
-#define PROPERTY_POISON            ((unsigned long long) (1LL << 5)) // match MR_POISON
-#define PROPERTY_ACID              ((unsigned long long) (1LL << 6)) // match MR_ACID
-#define PROPERTY_STONE             ((unsigned long long) (1LL << 7)) // match MR_STONE
-#define PROPERTY_SEE_INVISIBLE     ((unsigned long long) (1LL << 8))
-#define PROPERTY_LEVITATION        ((unsigned long long) (1LL << 9))
-#define PROPERTY_WATERWALKING      ((unsigned long long) (1LL << 10))
-#define PROPERTY_MAGICAL_BREATHING ((unsigned long long) (1LL << 11))
-#define PROPERTY_DISPLACEMENT      ((unsigned long long) (1LL << 12))
-#define PROPERTY_STRENGTH          ((unsigned long long) (1LL << 13))
-#define PROPERTY_FUMBLING          ((unsigned long long) (1LL << 14))
-#define PROPERTY_MAGICRES          ((unsigned long long) (1LL << 15))
-#define PROPERTY_REFLECTION        ((unsigned long long) (1LL << 16))
-#define PROPERTY_INVISIBLE         ((unsigned long long) (1LL << 17))
-#define PROPERTY_VISIBLE           ((unsigned long long) (1LL << 18))
-#define PROPERTY_BRILLIANCE        ((unsigned long long) (1LL << 19))
-#define PROPERTY_ESP               ((unsigned long long) (1LL << 20))
-#define PROPERTY_STUPIDITY         ((unsigned long long) (1LL << 21))
-#define PROPERTY_DEXTERITY         ((unsigned long long) (1LL << 22))
-#define PROPERTY_JUMPING           ((unsigned long long) (1LL << 23))
-#define PROPERTY_KICKING           ((unsigned long long) (1LL << 24))
-#define PROPERTY_STEALTH           ((unsigned long long) (1LL << 25))
-#define PROPERTY_VERYFAST          ((unsigned long long) (1LL << 26))
-#define PROPERTY_SLIPPERY          ((unsigned long long) (1LL << 27))
-#define PROPERTY_MAGIC             ((unsigned long long) (1LL << 28))
-#define PROPERTY_RANDOM_APPEARANCE ((unsigned long long) (1LL << 29))
-#define PROPERTY_CASTING_BONUS     ((unsigned long long) (1LL << 30))
-
 /* material */
 #define MATERIAL_WAX         ((unsigned int)(1 << 0))
 #define MATERIAL_VEGGY       ((unsigned int)(1 << 1))
