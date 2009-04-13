34bb0739d799b8fa056cf9c3694656fdb35ed4a7
Vidar Wahlberg <canidae@exent.net>
2009-04-13 21:06:25 +0200

    improved ReceivedItems

--- Events/ReceivedItems.h
+++ Events/ReceivedItems.h
@@ -1,12 +1,15 @@
 #ifndef _RECEIVED_ITEMS_H
 #define	_RECEIVED_ITEMS_H
 
+#include <vector>
 #include "Event.h"
+#include "../Item.h"
 
 namespace event {
 	class ReceivedItems {
 	public:
 		static const int id;
+		std::vector<Item> items;
 
 		ReceivedItems();
 		virtual ~ReceivedItems();
