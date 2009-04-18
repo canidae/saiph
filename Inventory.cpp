19564d487546200551eb7673030d0f18e92a07a0
Vidar <canidae@exent.net>
2009-04-18 21:25:32 +0200

    made add/remove item methods not inlined

--- Inventory.cpp
+++ Inventory.cpp
@@ -6,7 +6,41 @@
 using namespace event;
 using namespace std;
 
+
+/* define static variables */
+bool Inventory::updated = false;
+std::map<unsigned char, Item> Inventory::items;
+
+/* define private static variables */
+std::vector<unsigned char> Inventory::changed_items;
+
 /* methods */
+void Inventory::addItem(unsigned char key, const Item &item) {
+	if (item.count <= 0)
+		return;
+	Debug::notice() << INVENTORY_DEBUG_NAME << "Adding " << item.count << " " << item.name << " to inventory slot " << key << std::endl;
+	if (items.find(key) != items.end()) {
+		/* existing item, add amount */
+		items[key].count += item.count;
+	} else {
+		/* new item */
+		items[key] = item;
+	}
+}
+
+void Inventory::removeItem(unsigned char key, const Item &item) {
+	if (item.count <= 0)
+		return;
+	std::map<unsigned char, Item>::iterator i = items.find(key);
+	if (i == items.end())
+		return;
+	Debug::notice() << INVENTORY_DEBUG_NAME << "Removing " << item.count << " " << item.name << " from inventory slot " << key << std::endl;
+	if (i->second.count > item.count)
+		i->second.count -= item.count; // we got more than we remove
+	else    
+		items.erase(i); // removing all we got
+}
+
 void Inventory::parseMessages(const string &messages) {
 	if (messages.find(MESSAGE_NOT_CARRYING_ANYTHING) != string::npos || messages.find(MESSAGE_NOT_CARRYING_ANYTHING_EXCEPT_GOLD) != string::npos) {
 		/* we're not carrying anything */
--- Inventory.h
+++ Inventory.h
@@ -18,44 +18,10 @@ public:
 	static std::map<unsigned char, Item> items;
 
 	static void addItem(unsigned char key, const Item &item);
-	static void parseMessages(const std::string &messages);
 	static void removeItem(unsigned char key, const Item &item);
+	static void parseMessages(const std::string &messages);
 
 private:
 	static std::vector<unsigned char> changed_items;
 };
-
-/* inline methods */
-inline void Inventory::addItem(unsigned char key, const Item &item) {
-	if (item.count <= 0)
-		return;
-	Debug::notice() << INVENTORY_DEBUG_NAME << "Adding " << item.count << " " << item.name << " to inventory slot " << key << std::endl;
-	if (items.find(key) != items.end()) {
-		/* existing item, add amount */
-		items[key].count += item.count;
-	} else {
-		/* new item */
-		items[key] = item;
-	}
-}
-
-inline void Inventory::removeItem(unsigned char key, const Item &item) {
-	if (item.count <= 0)
-		return;
-	std::map<unsigned char, Item>::iterator i = items.find(key);
-	if (i == items.end())
-		return;
-	Debug::notice() << INVENTORY_DEBUG_NAME << "Removing " << item.count << " " << item.name << " from inventory slot " << key << std::endl;
-	if (i->second.count > item.count)
-		i->second.count -= item.count; // we got more than we remove
-	else    
-		items.erase(i); // removing all we got
-}
-
-/* define static variables */
-bool Inventory::updated = false;
-std::map<unsigned char, Item> Inventory::items;
-
-/* define private static variables */
-std::vector<unsigned char> Inventory::changed_items;
 #endif
