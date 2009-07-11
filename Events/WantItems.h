3cd354e009f4313bdc40e0188940cd2b0ac746c2
Vidar Wahlberg <canidae@exent.net>
2009-07-11 02:45:53 +0200

    meh

--- Events/WantItems.h
+++ Events/WantItems.h
@@ -11,14 +11,16 @@
  * solution:
  * create a single event that just ask analyzers if they want the items.
  * in case of pickup, analyzers just selects the items they want.
- * in case of drop, we first select every item to be dropped, then analyzers select what _not_ to be dropped */
+ * in case of drop, we first "remove" all items in our inventory and let the analyzer "pick up" what they want.
+ * the analyzer set the "want" value how many they want of a given item and we set it to -1 when we drop for beatifying */
 namespace event {
 	class WantItems : public Event {
 	public:
 		static int id;
 		std::map<unsigned char, Item> items;
+		std::map<unsigned char, int> want;
 
-		WantItems() : items() {}
+		WantItems() : items(), want() {}
 		virtual ~WantItems() {}
 
 		virtual int getID() {return id;}
