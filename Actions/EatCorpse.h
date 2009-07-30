c83cb7e4d7571ad60655fd7964b41082e9bde796
Vidar Wahlberg <canidae@exent.net>
2009-07-30 21:16:01 +0200

    EatCorpse works in theory now :p

--- Actions/EatCorpse.h
+++ Actions/EatCorpse.h
@@ -8,7 +8,7 @@ namespace action {
 	public:
 		static int id;
 
-		EatCorpse(analyzer::Analyzer *analyzer, std::string corpse, int priority) : Action(analyzer), eat(std::string(1, 'e'), priority), corpse(corpse), answer_no(std::string(1, NO), PRIORITY_CONTINUE_ACTION), answer_yes(std::string(1, YES), PRIORITY_CONTINUE_ACTION) {}
+		EatCorpse(analyzer::Analyzer *analyzer, std::string corpse, int priority) : Action(analyzer), eat(std::string(1, 'e'), priority), corpse(corpse), answer_no(std::string(1, NO), PRIORITY_CONTINUE_ACTION), answer_yes(std::string(1, YES), PRIORITY_CONTINUE_ACTION), look(":", PRIORITY_CONTINUE_ACTION), escape(std::string(1, (char) 27), PRIORITY_CONTINUE_ACTION) {}
 		virtual ~EatCorpse() {}
 
 		virtual int getID() {return id;}
@@ -20,6 +20,7 @@ namespace action {
 		const std::string corpse;
 		const Command answer_no;
 		const Command answer_yes;
+		const Command look;
 		const Command escape;
 	};
 
@@ -36,7 +37,10 @@ namespace action {
 			return answer_yes;
 
 		case 3:
-			return answer_yes;
+			return look;
+
+		case 4:
+			return escape;
 
 		default:
 			return Action::noop;
@@ -45,14 +49,21 @@ namespace action {
 
 	inline void EatCorpse::updateAction(const std::string &messages) {
 		if (messages.find(MESSAGE_WHAT_TO_EAT) != std::string::npos) {
-			sequence = 3;
+			/* we got "what do you want to eat?", we shouldn't get this
+			 * as we want to eat a corpse on the ground. escape */
+			sequence = 4;
 		} else if (messages.find(MESSAGE_EAT_IT_2) != std::string::npos || messages.find(MESSAGE_EAT_ONE_2) != std::string::npos) {
+			/* asking if we should eat corpse on ground, check that it's the right corpse */
 			if (messages.find(corpse) != std::string::npos && (messages.find(MESSAGE_EAT_IT_1) != std::string::npos || messages.find(MESSAGE_EAT_ONE_1) != std::string::npos))
 				sequence = 2;
 			else
 				sequence = 1;
-		} else if (sequence == 1 || sequence == 2) {
-			sequence = 4;
+		} else if (sequence == 1 || sequence == 2 || sequence == 4) {
+			/* either ate a corpse or cancelled the eat request, make us look at ground */
+			sequence = 3;
+		} else if (sequence == 3) {
+			/* looked at ground, action is complete */
+			sequence = 5;
 		}
 	}
 }
