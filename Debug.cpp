f2e656c376e219766ee0003b0b61d9145ca3cbb7
Vidar Wahlberg <canidae@exent.net>
2009-10-18 17:16:59 +0200

    cleaned up Debug

--- Debug.cpp
+++ Debug.cpp
@@ -1,5 +1,79 @@
 #include "Debug.h"
+#include "World.h"
+
+#define DEBUG_CATEGORY_MAX_LENGTH 9
+#define DEBUG_INVENTORY "Inventory"
+#define DEBUG_BROADCAST "Broadcast"
+#define DEBUG_INFO "Info"
+#define DEBUG_NOTICE "Notice"
+#define DEBUG_WARNING"Warning"
+#define DEBUG_ERROR "Error"
+#define DEBUG_DATA "Data"
+
+using namespace std;
 
 /* define static variables */
-std::string Debug::timestamp;
-std::ofstream Debug::debugfile;
+string Debug::_timestamp;
+ofstream Debug::_debugfile;
+
+/* methods */
+void Debug::init(const string& file) {
+	_debugfile.open(file.c_str(), ios_base::trunc);
+}
+
+void Debug::destroy() {
+	_debugfile.close();
+}
+
+ofstream& Debug::analyzer(const string& name) {
+	return header(name);
+}
+
+ofstream& Debug::broadcast() {
+	return header(DEBUG_BROADCAST);
+}
+
+ofstream& Debug::info() {
+	return header(DEBUG_INFO);
+}
+
+ofstream& Debug::inventory() {
+	return header(DEBUG_INVENTORY);
+}
+
+ofstream& Debug::notice() {
+	return header(DEBUG_NOTICE);
+}
+
+ofstream& Debug::warning() {
+	return header(DEBUG_WARNING);
+}
+
+ofstream& Debug::error() {
+	return header(DEBUG_ERROR);
+}
+
+void Debug::rawCharArray(const char* data, int start, int stop) {
+	header(DEBUG_DATA);
+	for (int a = start; a < stop; ++a)
+		_debugfile << data[a];
+	_debugfile << endl;
+}
+
+/* private methods */
+ofstream& Debug::header(const string &category) {
+	_debugfile << "<T" << World::turn << "> " << printTime() << " [";
+	if (category.size() > DEBUG_CATEGORY_MAX_LENGTH)
+		_debugfile << category.substr(0, DEBUG_CATEGORY_MAX_LENGTH);
+	else
+		_debugfile << category << string(DEBUG_CATEGORY_MAX_LENGTH - category.size(), ' ');
+	_debugfile << "] ";
+	return _debugfile;
+}
+
+string& Debug::printTime() {
+	time_t rawtime;
+	time(&rawtime);
+	_timestamp = asctime(localtime(&rawtime));
+	return _timestamp.erase(_timestamp.size() - 1);
+}
--- Debug.h
+++ Debug.h
@@ -5,9 +5,7 @@ extern "C" {
 #include <sys/stat.h>
 }
 #include <fstream>
-#include <iostream>
 #include <string>
-#include "World.h"
 
 class Debug {
 public:
@@ -23,68 +21,10 @@ public:
 	static void rawCharArray(const char* data, int start, int stop);
 
 private:
-	static std::ofstream debugfile;
-	static std::string timestamp;
+	static std::ofstream _debugfile;
+	static std::string _timestamp;
 
+	static std::ofstream& header(const std::string& category);
 	static std::string& printTime();
 };
-
-/* inline methods */
-inline void Debug::init(const std::string& file) {
-	debugfile.open(file.c_str(), std::ios_base::trunc);
-}
-
-inline void Debug::destroy() {
-	debugfile.close();
-}
-
-inline std::ofstream& Debug::analyzer(const std::string& name) {
-	debugfile << "<T" << World::turn << "> " << printTime() << ": [" << name << "] ";
-	return debugfile;
-}
-
-inline std::ofstream& Debug::broadcast() {
-	debugfile << "<T" << World::turn << "> " << printTime() << ": [Broadcast] ";
-	return debugfile;
-}
-
-inline std::ofstream& Debug::info() {
-	debugfile << "<T" << World::turn << "> " << printTime() << ": [Info] ";
-	return debugfile;
-}
-
-inline std::ofstream& Debug::inventory() {
-	debugfile << "<T" << World::turn << "> " << printTime() << ": [Inventory] ";
-	return debugfile;
-}
-
-inline std::ofstream& Debug::notice() {
-	debugfile << "<T" << World::turn << "> " << printTime() << ": [Notice] ";
-	return debugfile;
-}
-
-inline std::ofstream& Debug::warning() {
-	debugfile << "<T" << World::turn << "> " << printTime() << ": [Warning] ";
-	return debugfile;
-}
-
-inline std::ofstream& Debug::error() {
-	debugfile << "<T" << World::turn << "> " << printTime() << ": [Error] ";
-	return debugfile;
-}
-
-inline void Debug::rawCharArray(const char* data, int start, int stop) {
-	debugfile << "<T" << World::turn << "> " << printTime() << ": [Data] ";
-	for (int a = start; a < stop; ++a)
-		debugfile << data[a];
-	debugfile << std::endl;
-}
-
-/* inline private methods */
-inline std::string& Debug::printTime() {
-	time_t rawtime;
-	time(&rawtime);
-	timestamp = asctime(localtime(&rawtime));
-	return timestamp.erase(timestamp.size() - 1);
-}
 #endif
