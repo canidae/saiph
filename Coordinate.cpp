e0587b2d509f8b7431fa0cd1a9b995a26d24dece
Vidar Wahlberg <canidae@exent.net>
2009-10-18 15:48:10 +0200

    definitions from Coordinate.h to Coordinate.cpp

--- Coordinate.cpp
+++ Coordinate.cpp
@@ -2,6 +2,48 @@
 
 using namespace std;
 
-ostream& operator<<(ostream& os, const Coordinate& c) {
+/* constructors/destructors */
+Coordinate::Coordinate(int level, int row, int col) : Point(row, col), _level(level) {
+}
+
+Coordinate::Coordinate(int level, const Point& point) : Point(point), _level(level) {
+}
+
+/* methods */
+int Coordinate::level() const {
+	return _level;
+}
+
+int Coordinate::level(int level) {
+	_level = level;
+	return this->level();
+}
+
+/* operator overloading */
+bool Coordinate::operator<(const Coordinate& c) const {
+	return (_level < c._level || (_level == c._level && row() < c.row()) || (_level == c._level && row() == c.row() && col() < c.col()));
+}
+
+bool Coordinate::operator>(const Coordinate& c) const {
+	return (_level > c._level || (_level == c._level && row() > c.row()) || (_level == c._level && row() == c.row() && col() > c.col()));
+}
+
+bool Coordinate::operator<=(const Coordinate& c) const {
+	return (_level < c._level || (_level == c._level && row() < c.row()) || (_level == c._level && row() == c.row() && col() <= c.col()));
+}
+
+bool Coordinate::operator>=(const Coordinate& c) const {
+	return (_level > c._level || (_level == c._level && row() > c.row()) || (_level == c._level && row() == c.row() && col() >= c.col()));
+}
+
+bool Coordinate::operator==(const Coordinate& c) const {
+	return (_level == c._level && row() == c.row() && col() == c.col());
+}
+
+bool Coordinate::operator!=(const Coordinate& c) const {
+	return (_level != c._level || row() != c.row() || col() != c.col());
+}
+
+ostream & operator<<(ostream& os, const Coordinate& c) {
 	return os << "(" << c.level() << ", " << c.row() << ", " << c.col() << ")";
 }
--- Coordinate.h
+++ Coordinate.h
@@ -5,49 +5,22 @@
 
 class Coordinate : public Point {
 public:
+	Coordinate(int level = -1, int row = -1, int col = -1);
+	Coordinate(int level, const Point& point = Point());
 
-	Coordinate(int level = -1, int row = -1, int col = -1) : Point(row, col), _level(level) {
-	}
+	int level() const;
+	int level(int level);
 
-	Coordinate(int level, const Point& point = Point()) : Point(point), _level(level) {
-	}
-
-	int level() const {
-		return _level;
-	}
-
-	int level(int level) {
-		_level = level;
-		return this->level();
-	}
-
-	bool operator<(const Coordinate& c) const {
-		return (_level < c._level || (_level == c._level && row() < c.row()) || (_level == c._level && row() == c.row() && col() < c.col()));
-	}
-
-	bool operator>(const Coordinate& c) const {
-		return (_level > c._level || (_level == c._level && row() > c.row()) || (_level == c._level && row() == c.row() && col() > c.col()));
-	}
-
-	bool operator<=(const Coordinate& c) const {
-		return (_level < c._level || (_level == c._level && row() < c.row()) || (_level == c._level && row() == c.row() && col() <= c.col()));
-	}
-
-	bool operator>=(const Coordinate& c) const {
-		return (_level > c._level || (_level == c._level && row() > c.row()) || (_level == c._level && row() == c.row() && col() >= c.col()));
-	}
-
-	bool operator==(const Coordinate& c) const {
-		return (_level == c._level && row() == c.row() && col() == c.col());
-	}
-
-	bool operator!=(const Coordinate& c) const {
-		return (_level != c._level || row() != c.row() || col() != c.col());
-	}
+	bool operator<(const Coordinate& c) const;
+	bool operator>(const Coordinate& c) const;
+	bool operator<=(const Coordinate& c) const;
+	bool operator>=(const Coordinate& c) const;
+	bool operator==(const Coordinate& c) const;
+	bool operator!=(const Coordinate& c) const;
 
 private:
 	int _level;
 };
 
-std::ostream& operator<<(std::ostream& os, const Coordinate& c);
+std::ostream & operator<<(std::ostream& os, const Coordinate& c);
 #endif
