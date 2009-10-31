80c491216791b4123b88014d50d25f6a8f063bc2
Vidar Wahlberg <canidae@exent.net>
2009-10-31 16:19:57 +0100

    don't rest so much if there are no nearby monsters

--- Analyzers/Health.cpp
+++ Analyzers/Health.cpp
@@ -1,4 +1,6 @@
 #include "Health.h"
+
+#include <stdlib.h>
 #include "../Saiph.h"
 #include "../World.h"
 #include "../Actions/Engrave.h"
@@ -37,7 +39,19 @@ void Health::analyze() {
 		}
 		if (!doing_something) {
 			/* we're not going to quaff/pray, set resting = true to make her elbereth/rest instead */
-			_resting = true;
+			if (Saiph::hitpoints() > Saiph::hitpointsMax() * 4 / 7 && Saiph::hitpointsMax() > 42) {
+				/* if we don't see any monsters, then we won't rest for so long */
+				bool monster_nearby = false;
+				for (map<Point, Monster>::iterator m = World::level().monsters().begin(); m != World::level().monsters().end(); ++m) {
+					if (m->second.visible() || (abs(Saiph::position().row() - m->first.row()) < 5 && abs(Saiph::position().col() - m->first.col()) < 5)) {
+						monster_nearby = true;
+						break;
+					}
+				}
+				_resting = monster_nearby;
+			} else {
+				_resting = true;
+			}
 		}
 	}
 	if (Saiph::confused() || Saiph::hallucinating() || Saiph::foodpoisoned() || Saiph::ill() || Saiph::stunned()) {
@@ -86,6 +100,7 @@ void Health::analyze() {
 	if (_prev_str < Saiph::strength() || _prev_dex < Saiph::dexterity() || _prev_con < Saiph::constitution() || _prev_int < Saiph::intelligence() || _prev_wis < Saiph::wisdom() || _prev_cha < Saiph::charisma()) {
 		/* TODO: we lost some stats. apply unihorn */
 	}
+
 	/* set previous stat values */
 	_prev_str = Saiph::strength();
 	_prev_dex = Saiph::dexterity();
