a1b9106f4dc20a755c546979b41cc60a3e19b1db
RawrMage <rawrmage@gmail.com>
2011-04-03 12:45:35 -0400

    Hopefully made saiph respond to wish prompts so it won't waste wishes.

--- src/Analyzers/Wish.cpp
+++ src/Analyzers/Wish.cpp
@@ -1,6 +1,8 @@
 #include "Analyzers/Wish.h"
 
 #include <string>
+#include "Actions/Answer.h"
+#include "Inventory.h"
 #include "Globals.h"
 #include "Saiph.h"
 #include "World.h"
@@ -15,37 +17,36 @@ Wish::Wish(Saiph* saiph) : Analyzer("Wish"), saiph(saiph), wand_of_wishing_key(0
 
 /* methods */
 void Wish::parseMessages(const string& messages) {
-	if (saiph->inventory_changed) {
-		wand_of_wishing_key = 0;
-		for (map<unsigned char, Item>::iterator i = saiph->inventory.begin(); i != saiph->inventory.end(); ++i) {
-			if (i->second.name == "wand of wishing") {
-				wand_of_wishing_key = i->first;
-			}
-		}
-	}
+	//if (saiph->inventory_changed) {
+		//wand_of_wishing_key = 0;
+		//for (map<unsigned char, Item>::iterator i = saiph->inventory.begin(); i != saiph->inventory.end(); ++i) {
+			//if (i->second.name == "wand of wishing") {
+				//wand_of_wishing_key = i->first;
+			//}
+		//}
+	//}
 	if (messages.find(MESSAGE_FOR_WHAT_DO_YOU_WISH, 0) != string::npos) {
-		haveMR = saiph->world->player.extrinsics & PROPERTY_MAGICRES;
+		haveMR = saiph->extrinsics() & PROPERTY_MAGICRES;
 		if (haveMR) {
 			MRarmor = wearing("gray dragon scale mail") || wearing("gray dragon scales");
 			MRcloak = wearing("cloak of magic resistance");
 		} else
 			MRarmor = MRcloak = false;
-		haveReflection = saiph->world->player.extrinsics & PROPERTY_REFLECTION;
+		haveReflection = saiph->extrinsics() & PROPERTY_REFLECTION;
 		if (haveReflection) {
 			reflectionArmor = wearing("silver dragon scale mail") || wearing("silver dragon scales");
 			reflectionAmulet = wearing("amulet of reflection");
 			reflectionShield = wearing("shield of reflection");
 		} else
 			reflectionArmor = reflectionAmulet = reflectionShield = false;
-		command = "3 blessed greased fixed +3 " + selectWish();
-		Debug::notice(saiph->last_turn) << WISH_DEBUG_NAME << "Wishing for " << command << endl;
+		string command = "blessed greased fixed +3 " + selectWish();
+		Debug::notice() << WISH_DEBUG_NAME << "Wishing for " << command << endl;
 		command.append("\n");
-		priority = PRIORITY_CONTINUE_ACTION;
+		World::queueAction(static_cast<action::Action*> (new action::Answer(this, command)));
 		return;
 	} else if (zapping_wand && messages.find(MESSAGE_WHAT_TO_ZAP, 0) != string::npos) {
 		zapping_wand = false;
-		command = wand_of_wishing_key;
-		priority = PRIORITY_CONTINUE_ACTION;
+		//string command = wand_of_wishing_key;
 		return;
 	}
 }
@@ -54,13 +55,13 @@ void Wish::analyze() {
 	//if we have a wand of wishing, we need to decide if we should zap
 	//TODO: get charging and recharge it; currently it'll wrest it before charging
 	//since it has no idea of "this wand is empty"
-	if (wand_of_wishing_key != 0) {
-		if (!haveReflection || !haveMR || !wearing("speed boots") || !wearing("gauntlets of power")) {
-			zapping_wand = true;
-			command = ZAP_WAND;
-			priority = PRIORITY_WISH_ZAP_WAND;
-		}
-	}
+	//if (wand_of_wishing_key != 0) {
+		//if (!haveReflection || !haveMR || !wearing("speed boots") || !wearing("gauntlets of power")) {
+			//zapping_wand = true;
+			//command = ZAP_WAND;
+			//priority = PRIORITY_WISH_ZAP_WAND;
+		//}
+	//}
 }
 
 string Wish::selectWish() {
@@ -86,8 +87,8 @@ string Wish::selectWish() {
 }
 
 bool Wish::wearing(const string& name) {
-	for (map<unsigned char, Item>::iterator i = saiph->inventory.begin(); i != saiph->inventory.end(); ++i)
-		if (i->second.name.find(name) != string::npos)
+	for (map<unsigned char, Item>::iterator i = Inventory::items().begin(); i != Inventory::items().end(); ++i)
+		if (i->second.name().find(name) != string::npos)
 			return true;
 	return false;
 }
