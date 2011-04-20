ae8bc9dd9e77defdcf3a1ed9052d2885992a7d83
Stefan O'Rear <stefanor@cox.net>
2011-04-20 00:13:11 -0700

    Check questions more robustly in ApplyInDirection

--- include/Actions/ApplyInDirection.h
+++ include/Actions/ApplyInDirection.h
@@ -41,10 +41,11 @@ namespace action {
 				_sequence = 1;
 			} else if (messages.find(MESSAGE_DIG_DIRECTION) != std::string::npos) {
 				_sequence = 2;
-			} else if (_sequence == 2) {
+			} else {
 				/* mark inventory dirty if update_inventory is true */
 				if (_update_inventory)
 					Inventory::update();
+				if (_sequence != 2) failed();
 				_sequence = 3;
 			}
 		}
