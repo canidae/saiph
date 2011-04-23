7f941f8eb351f4a142351f692f5a4828007d69c6
Vidar Wahlberg <canidae@exent.net>
2011-04-23 15:29:02 +0200

    Beatitude analyzer sets variable priority now (may not work well)

--- src/Analyzers/Beatitude.cpp
+++ src/Analyzers/Beatitude.cpp
@@ -36,7 +36,7 @@ void Beatitude::analyze() {
 	if (tile.direction() == NOWHERE)
 		World::setAction(static_cast<action::Action*> (new action::Drop(this, _max_priority, false)));
 	else
-		World::setAction(static_cast<action::Action*> (new action::Move(this, tile, _max_priority)));
+		World::setAction(static_cast<action::Action*> (new action::Move(this, tile, action::Move::calculatePriority(_max_priority, tile.cost()))));
 }
 
 void Beatitude::onEvent(event::Event* const event) {
